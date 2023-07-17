51e7917228d8f3066f6d27b0cee048e72caf9150
Ga1156 <gauravpardeshi068@gmail.com>
2023-07-17 18:30:43 +0530

    done

---
+++ Evaluation/Python_Que/main.py
@@ -0,0 +1,48 @@
+
+# Question 4
+
+# def check_palindrome():
+
+#     str="Masai"
+#     reversed_str==number[::-1]
+
+#     if(str==reversed_str):
+#         return True
+#     else:
+#         return False
+    
+
+# Question 5
+
+# company = {
+#     'employees': {
+#         'John': {'age': 35, 'job_title': 'Manager'},
+#         'Emma': {'age': 28, 'job_title': 'Software Engineer'},
+#         'Kelly': {'age': 41, 'job_title': 'Senior Developer'},
+#         'Sam': {'age': 30, 'job_title': 'Software Engineer'},
+#         'Mark': {'age': 37, 'job_title': 'Senior Manager'},
+#         'Sara': {'age': 32, 'job_title': 'Software Engineer'},
+#     }
+# }
+
+# def get_employee(employees):
+
+#     age=0
+#     count=0
+
+#     for employee in employees.values():
+#         if employee.get('job_title','').startswith('S') :
+#             age=employee.get('age')
+#             if age is not None:
+#                 age+=age 
+#                 counr+=1 
+
+
+#     if count==0:
+#         return 0
+    
+
+
+#     average=age/count 
+#     return average
+# print(get_employee(company))
